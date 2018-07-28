4ad5d3233a0359e86b309de846d8a653b9f91d44
Ferdis17 <fmuriyesu@users.noreply.github.com>
2018-07-28 18:40:00 -0400

    PlayRPS
    It's done but all cases are not tested.

--- ford/src/problem1/PlayRPS.java
+++ ford/src/problem1/PlayRPS.java
@@ -2,9 +2,33 @@ package problem1;
 
 public class PlayRPS {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static final String rps1 = "ROCK";
+	public static final String rps2 = "PAPER";
+	public static final String rps3 = "Scissors";
 
+	public static void main(String args[]) {
+		
+		System.out.println(playRPS(rps1, rps2));
+		System.out.println(playRPS(rps2, rps3));
+		System.out.println(playRPS(rps1, rps3));
+		System.out.println(playRPS(rps1, rps1));
+
+	}
+
+	public static String playRPS(String player1Throw, String player2Throw) {
+
+		if (player1Throw.equals(rps1) && player2Throw.equals(rps2)) {
+			return "Player 2 wins";
+		}
+
+		else if (player1Throw.equals(rps2) && player2Throw.equals(rps3)) {
+			return "Player 1 wins";
+		}
+
+		else if (player1Throw.equals(rps1) && player2Throw.equals(rps3)) {
+			return "Player 1 wins";
+		}
+		return "It's Tie";
 	}
 
 }
